1cebd5c3569cf9846b4b699eda93109ba967cb77
ANGEL\Remya <menon.remya@gmail.com>
2017-05-23 22:54:25 +0530

    minor change

--- spring-kafka-helloworld/src/main/java/com/codenotfound/kafka/MesgImpl.java
+++ spring-kafka-helloworld/src/main/java/com/codenotfound/kafka/MesgImpl.java
@@ -1,8 +1,10 @@
 package com.codenotfound.kafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.codenotfound.kafka.producer.Sender;
 
 @RestController
@@ -11,10 +13,13 @@ public class MesgImpl {
 	@Autowired
 	Sender sender;
 	
+	@Value("${kafka.topic.helloworld}")
+	String topicname;
+	
 	@RequestMapping("/send")
 	public String sendmsg()
 	{
-		sender.send("helloworld", "Hi..This is Kafka testing");
+		sender.send(topicname, "Hi..This is Kafka testing");
 		return "msg sent!!";
 	}
 	
